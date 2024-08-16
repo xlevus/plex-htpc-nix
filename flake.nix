20f480fd9e6ac67812b9c4313b0103a33f057efa
zimbatm <zimbatm@zimbatm.com>
2024-08-16 11:38:48 +0200

    fix .#packages.aarch64-linux.htpc-1-sdImage
    * Replace the nixos-generators wrapper with direct usage to simplify the
      reasoning a bit.
    * Split the machine configuration from the sd-card generation, to make
      things a bit simpler as well.
    * Add nixos-hardware for the Rasperri Pi 4 hardware configuration.
    * Add necessary workarounds.

--- flake.nix
+++ flake.nix
@@ -5,18 +5,26 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware";
     };
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-    }
+    };
   };
 
-  outputs = { nixpkgs, disko, nixos-generators, home-manager, nix-flatpak, ... }:
+  outputs = { self, nixpkgs, disko, home-manager, nix-flatpak, nixos-hardware, ... }:
+  let
+    eachSystem = f:
+      nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"] (system:
+        f {
+          inherit system;
+          pkgs = nixpkgs.legacyPackages.${system};
+        }
+      );
+  in
   {
     nixosConfigurations = {
 
@@ -30,15 +38,57 @@
         ];
       };
 
-    };
-
-    packages.x86_64-linux = {
-      htpc-1 = nixos-generators.nixosGenerate {
-        system = "armv7l-linux";
+      # https://wiki.nixos.org/wiki/NixOS_on_ARM/Raspberry_Pi_4
+      "htpc-1" = nixpkgs.lib.nixosSystem {
         modules = [
-          ./common/users.nix
+          # TODO: move this to a configuration.nix file
+          ({ pkgs, ... }: {
+            imports = [
+              home-manager.nixosModules.home-manager
+              nixos-hardware.nixosModules.raspberry-pi-4
+              ./htpc/user.nix
+              "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+            ];
+
+            nixpkgs.hostPlatform = "aarch64-linux";
+
+            # Fix missing modules
+            # https://github.com/NixOS/nixpkgs/issues/154163
+            nixpkgs.overlays = [
+              (final: super: {
+                makeModulesClosure = x:
+                  super.makeModulesClosure (x // { allowMissing = true; });
+              })
+            ];
+
+            hardware = {
+              raspberry-pi."4".apply-overlays-dtmerge.enable = true;
+              deviceTree = {
+                enable = true;
+                filter = "*rpi-4-*.dtb";
+              };
+            };
+
+            console.enable = false;
+
+            environment.systemPackages = with pkgs; [
+              libraspberrypi
+              raspberrypi-eeprom
+            ];
+
+            system.stateVersion = "24.05";
+          })
         ];
       };
+
     };
+
+    packages = eachSystem ({ system, ... }: {
+      htpc-1-sdImage = (self.nixosConfigurations.htpc-1.extendModules {
+        # Configure cross-compilation
+        modules = [ { nixpkgs.buildPlatform.system = system; } ];
+        # Expose the sdImage brought in by the sd-image-aarch64.nix profile
+      }).config.system.build.sdImage;
+    });
   };
 }
